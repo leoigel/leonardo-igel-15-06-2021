208584a7280a7cc54a7ab624bd45d27bf78b880b
Leonardo <leoigeltiko15@gmail.com>
2021-06-19 13:21:47 +0300

    loading component was created

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <md-progress-spinner md-mode="indeterminate" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProgressSpinnerIndeterminate'
+  }
+</script>
+
+<style scoped>
+  .md-progress-spinner {
+    margin: 24px;
+  }
+</style>
